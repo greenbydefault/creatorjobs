d4b79c885135e9e82221463c72b6e03e82fdbf7f
greenbydefault <113526876+greenbydefault@users.noreply.github.com>
2025-04-07 08:50:14 +0200

    Update bundle-v3.2.js

--- src/bundle-v3.2.js
+++ src/bundle-v3.2.js
@@ -88,7 +88,7 @@
     uiManager: './modules/ui-manager-1.4.js',
     videoFeedApp: './modules/video-feed-app-1.4.js',
     videoEditApp: './modules/video-edit-app-1.2.js',
-    videoUploadApp: './modules/video-upload-app-1.4.js'
+    videoUploadApp: './modules/video-upload-app-1.5.js'
   };
   
   // Debugging-Ausgabe

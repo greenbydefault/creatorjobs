d4c2cd40d3ad8e1441403099a7738e807818ef21
greenbydefault <113526876+greenbydefault@users.noreply.github.com>
2025-04-07 10:39:04 +0200

    Update and rename bundle-v3.9.js to bundle-v4.0.js

--- src/bundle-v4.0.js
+++ src/bundle-v4.0.js
@@ -85,8 +85,8 @@
     memberApi: './modules/member-api-1.7.js',
     uploadcare: './modules/uploadcare-1.4.js',
     memberstack: './modules/memberstack-1.1.js',
-    uiManager: './modules/ui-manager-1.4.js',
-    videoFeedApp: './modules/video-feed-app-1.5.js',
+    uiManager: './modules/ui-manager-1.5.js',
+    videoFeedApp: './modules/video-feed-app-1.6.js',
     videoEditApp: './modules/video-edit-app-1.3.js',
     videoUploadApp: './modules/video-upload-app-1.5.js',
     diagnostics: './modules/debug-diagnostics.js'

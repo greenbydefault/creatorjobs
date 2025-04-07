ebb6f9369e3e6101fb4c8e53f76dea96dd77d493
greenbydefault <113526876+greenbydefault@users.noreply.github.com>
2025-04-07 10:20:56 +0200

    Update and rename bundle-v3.8.js to bundle-v3.9.js

--- src/bundle-v3.9.js
+++ src/bundle-v3.9.js
@@ -79,7 +79,7 @@
   // Modul-Pfade
   const MODULE_PATHS = {
     debug: './modules/debug-1.1.js',
-    cache: './modules/cache-1.5.js',
+    cache: './modules/cache-1.6.js',
     apiService: './modules/api-service-1.0.js',
     videoApi: './modules/video-api-1.4.js',
     memberApi: './modules/member-api-1.7.js',

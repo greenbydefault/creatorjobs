56f840cafb3d32ecf0ff31058b3219576f6fadcc
greenbydefault <113526876+greenbydefault@users.noreply.github.com>
2025-06-04 08:57:15 +0200

    Update and rename index-1.0.52.js to index-1.0.53.js

--- brands/campaign/src/index-1.0.53.js
+++ brands/campaign/src/index-1.0.53.js
@@ -33,7 +33,7 @@ import './core/favoriteService.js';
 import './core/bookingService.js';
 
 // Hauptanwendungslogik, die viele der obigen Module orchestriert
-import './appLogic-1.9.js';
+import './appLogic-2.0.js';
 
 // Initialisierungsskript, das die appLogic startet
 import './main-1.2.js';
